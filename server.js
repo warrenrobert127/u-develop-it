6cc2c6a69b5e27bbca9d73c512f7f5e72c26a060
Robert Warren <2110981@wholefoods.com>
2021-12-06 21:49:48 -0600

    update server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -7,13 +8,35 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'mysql123',
+        database: 'election'
+    });
+
+db.connect(function () {
+    console.log('Connected to the election database.')
+  
+}
+);
 
 
-  // Default response for any other request (Not Found)
-app.use((req, res) => {
-    res.status(404).end();
-  });
+
+// Default response for any other request (Not Found)
+//app.use("/", (req, res) => {
+   /* db.query("select * from candidates;", function (err, data) {
+        if (err) throw err;
+        console.log(data)
+    }) */
+   
+    //res.status(404).end();
+//});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-  });
+});
